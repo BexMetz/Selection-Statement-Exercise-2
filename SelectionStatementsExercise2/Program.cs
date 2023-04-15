c18da4bef6c3e320b3e5a2e6a2ae1f77f4380c2a
Bex <bexmetzger@gmail.com>
2023-04-15 13:35:33 -0700

    subject switch

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello!");
+            Console.WriteLine("What is your favorite subject in school?");
+
+            string favoriteSubject = Console.ReadLine();
+
+            
+            switch (favoriteSubject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("Thats a tough one!");
+                    break;
+                case "science":
+                    Console.WriteLine("So interesting!");
+                    break;
+                case "language":
+                    Console.WriteLine("Habla espanol?");
+                    break;
+                case "history":
+                    Console.WriteLine("Is told by those who win.");
+                    break;
+                case "art":
+                    Console.WriteLine("That's my favorite too!");
+                    break;
+                default:
+                    Console.WriteLine("That's not a subject at any school I went to.");
+                    break;
+            }
         }
     }
 }
